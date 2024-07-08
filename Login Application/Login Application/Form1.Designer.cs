6d37a50be7ff3c2be62b093dfac6fe4b45b7aefa
RichardDjirackor <rdjirackor@st.ug.edu.gh>
2024-07-08 13:16:14 +0000

    Login application (details not centered yet)

---
+++ Login Application/Login Application/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace LoginApp
+{
+    partial class Form1
+    {
+        private IContainer components = null;
+        private TextBox usernameTextBox;
+        private TextBox passwordTextBox;
+        private Button loginButton;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(38, 41);
+            this.usernameTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(151, 20);
+            this.usernameTextBox.TabIndex = 0;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(38, 81);
+            this.passwordTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(151, 20);
+            this.passwordTextBox.TabIndex = 1;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(75, 122);
+            this.loginButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 24);
+            this.loginButton.TabIndex = 2;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 304);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.usernameTextBox);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Login Screen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ Login Application/Login Application/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LoginApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(Form1_Load);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CenterControls();
+        }
+
+        private void CenterControls()
+        {
+            usernameTextBox.Left = (this.ClientSize.Width - usernameTextBox.Width) / 2;
+            usernameTextBox.Top = (this.ClientSize.Height / 2) - 50;
+
+            passwordTextBox.Left = (this.ClientSize.Width - passwordTextBox.Width) / 2;
+            passwordTextBox.Top = usernameTextBox.Bottom + 10;
+
+            loginButton.Left = (this.ClientSize.Width - loginButton.Width) / 2;
+            loginButton.Top = passwordTextBox.Bottom + 10;
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            string username = usernameTextBox.Text;
+            string password = passwordTextBox.Text;
+
+            MessageBox.Show($"Username: {username}\nPassword: {password}", "Login Information");
+        }
+    }
+}
