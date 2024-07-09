c81d478055f27b136ae99c6914fac1aa7c9999e5
RichardDjirackor <rdjirackor@st.ug.edu.gh>
2024-07-09 06:48:55 +0000

    Done

---
+++ DrawingApp/DrawingApp/Form1.Designer.cs
@@ -0,0 +1,54 @@
+﻿namespace DrawingApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.canvasPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // canvasPanel
+            // 
+            this.canvasPanel.BackColor = System.Drawing.Color.White;
+            this.canvasPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.canvasPanel.Location = new System.Drawing.Point(0, 0);
+            this.canvasPanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.canvasPanel.Name = "canvasPanel";
+            this.canvasPanel.Size = new System.Drawing.Size(899, 560);
+            this.canvasPanel.TabIndex = 0;
+            this.canvasPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.canvasPanel_Paint);
+            this.canvasPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.canvasPanel_MouseDown);
+            this.canvasPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.canvasPanel_MouseMove);
+            this.canvasPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.canvasPanel_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 560);
+            this.Controls.Add(this.canvasPanel);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Drawing App";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel canvasPanel;
+    }
+}
---
+++ DrawingApp/DrawingApp/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DrawingApp
+{
+    public partial class Form1 : Form
+    {
+        private bool drawing = false;
+        private Point startPoint, endPoint;
+        private List<Tuple<Point, Point>> lines = new List<Tuple<Point, Point>>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void canvasPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                drawing = true;
+                startPoint = e.Location;
+            }
+        }
+
+        private void canvasPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drawing)
+            {
+                endPoint = e.Location;
+                canvasPanel.Invalidate();
+            }
+        }
+
+        private void canvasPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                drawing = false;
+                lines.Add(new Tuple<Point, Point>(startPoint, endPoint));
+            }
+        }
+
+        private void canvasPanel_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (var line in lines)
+            {
+                e.Graphics.DrawLine(Pens.Black, line.Item1, line.Item2);
+            }
+
+            if (drawing)
+            {
+                e.Graphics.DrawLine(Pens.Red, startPoint, endPoint);
+            }
+        }
+    }
+}
